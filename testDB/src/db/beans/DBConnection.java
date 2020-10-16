766353a7da61011de56069c5109585c33a377a4a
PSH <slow_going77@naver.com>
2020-10-16 16:20:23 +0900

    testDB

---
+++ testDB/src/db/beans/DBConnection.java
@@ -0,0 +1,21 @@
+package db.beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	public static Connection getConnection() throws Exception
+	{
+		System.out.println("DB연결시도");
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","1234");	
+	}
+	
+	public static Connection getConnection(String ip, int port, String db, String user, String pw) throws Exception
+	{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		return DriverManager.getConnection("jdbc:oracle:thin:@"+ip+":"+port+":"+db,user,pw);
+		
+	}
+
+}
